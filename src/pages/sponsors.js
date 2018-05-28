ae8939b6c348dd0d4015f47f14ed4d21ed464cfb
jamie <jamie.parkinson@techwillsaveus.com>
2018-05-28 11:57:42 +0100

    Add basic page & badge component with gql query

---
+++ src/pages/sponsors.js
@@ -0,0 +1,75 @@
+import React, { Fragment } from 'react'
+import styled from 'styled-components'
+import { Container } from '../components/grid/'
+import SponsorBadge from '../templates/sponsors/SponsorBadge'
+
+const Body = styled.div`
+  padding: 60px 0;
+  border-bottom: 1px solid rgba(203, 203, 203, 0.5);
+`
+
+const SponsorsList = styled.div`
+  padding: 60px 0;
+`
+
+const selectSponsors = data =>
+  data.allContentfulSponsor.edges.map(({ node }) => ({
+    name: node.sponsorName,
+    url: node.sponsorUrl,
+    logo: node.sponsorLogo && node.sponsorLogo.sizes.src,
+    level: node.sponsorLevel,
+  }))
+
+const Sponsors = ({ data }) => (
+  <Fragment>
+    <div>Banner goes here</div>
+    <Container>
+      <Body>
+        <h2>Be part of something</h2>
+        <p>
+          Scelerisque a parturient consectetur dui erat a ligula a a id
+          imperdiet elit scelerisque ipsum cubilia ac a. Malesuada elementum a
+          vestibulum fermentum sodales ullamcorper a vestibulum vulputate
+          nascetur nec felis ullamcorper ut quis at a purus vestibulum eu
+          parturient parturient condimentum vitae faucibus. Ultrices a mi quis
+          ullamcorper ullamcorper vestibulum etiam a imperdiet egestas orci
+          condimentum orci non arcu adipiscing mus a ac at facilisis quis.
+        </p>
+      </Body>
+      <SponsorsList>
+        <h1>Our main 2018 partners</h1>
+        <p>
+          A huge thank you to our main partners for their continued support.
+        </p>
+        <div>
+          {data &&
+            selectSponsors(data).map(sponsor => (
+              <SponsorBadge key={sponsor.name} {...sponsor} />
+            ))}
+        </div>
+      </SponsorsList>
+    </Container>
+  </Fragment>
+)
+
+export const query = graphql`
+  query sponsorsQuery {
+    allContentfulSponsor(filter: {}) {
+      distinct
+      edges {
+        node {
+          sponsorName
+          sponsorUrl
+          sponsorLogo {
+            sizes(maxHeight: 84) {
+              src
+            }
+          }
+          sponsorLevel
+        }
+      }
+    }
+  }
+`
+
+export default Sponsors
---
+++ src/templates/sponsors/SponsorBadge.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled, { css } from 'styled-components'
+
+const Badge = styled.div`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f9f9f9;
+  ${({ level }) =>
+    level === 'Headline'
+      ? css`
+          height: 175px;
+          width: 293px;
+        `
+      : css`
+          height: 110px;
+          width: 185px;
+        `};
+`
+
+const BadgeImage = styled.img`
+  max-height: ${({ level }) => (level === 'Headline' ? '84px' : '52px')};
+  max-width: ${({ level }) => (level === 'Headline' ? '220px' : '95px')};
+`
+
+const SponsorBadge = ({ logo, name, url, level }) => (
+  <Badge level={level}>
+    <a href={url} target="_blank">
+      <BadgeImage src={logo} alt={name} level={level} />
+    </a>
+  </Badge>
+)
+
+SponsorBadge.propTypes = {
+  level: PropTypes.oneOf(['Headline', 'Gold', 'Silver', 'Bronze']).isRequired,
+}
+
+export default SponsorBadge
