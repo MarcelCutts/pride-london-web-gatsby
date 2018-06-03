1ccabb1101f4de9de12802c151b4127d6c5684a4
Natalie Mclaren <natalielmclaren@hotmail.com>
2018-06-03 23:34:05 +0100

    Combine background properties into shorthand rule

--- src/components/accordion/accordionWrapper.js
+++ src/components/accordion/accordionWrapper.js
@@ -5,7 +5,7 @@ import chevronUp from '../../theme/assets/images/icon-chevron-up.svg'
 const AccordionWrapper = styled.div`
   .accordion__body {
     padding-top: 0 !important;
-    background-color: ${props => props.theme.colors.lightGrey} !important;
+    background: ${props => props.theme.colors.lightGrey};
     animation: none !important;
 
     p {
@@ -21,10 +21,8 @@ const AccordionWrapper = styled.div`
     padding: 20px !important;
     border-radius: 4px;
     outline: none;
-    background-color: ${props => props.theme.colors.lightGrey} !important;
-    background-image: url(${chevronDown});
-    background-repeat: no-repeat;
-    background-position: 94%;
+    background: url(${chevronDown}) ${props => props.theme.colors.lightGrey}
+      no-repeat 94%;
 
     h3 {
       line-height: 20px;
@@ -48,8 +46,8 @@ const AccordionWrapper = styled.div`
     border-right: 2px solid ${props => props.theme.colors.eucalyptusGreen} !important;
     border-bottom-left-radius: 0 !important;
     border-bottom-right-radius: 0 !important;
-    background-color: ${props => props.theme.colors.white} !important;
-    background-image: url(${chevronUp});
+    background: url(${chevronUp}) ${props => props.theme.colors.white} no-repeat
+      94%;
 
     h3 {
       color: ${props => props.theme.colors.indigo};
@@ -62,7 +60,7 @@ const AccordionWrapper = styled.div`
     border-right: 2px solid ${props => props.theme.colors.eucalyptusGreen} !important;
     border-bottom-left-radius: 4px !important;
     border-bottom-right-radius: 4px !important;
-    background-color: ${props => props.theme.colors.white} !important;
+    background-color: ${props => props.theme.colors.white};
   }
 
   .accordion__item + .accordion__item {
