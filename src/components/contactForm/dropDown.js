dd656eb21c9951f5657dc492cb932ce07b52a424
Natalie Mclaren <natalielmclaren@hotmail.com>
2018-06-03 15:14:22 +0100

    Add a hidden input and bind drop down value to it

--- src/components/contactForm/dropDown.js
+++ src/components/contactForm/dropDown.js
@@ -113,6 +113,12 @@ class DropDown extends React.Component {
         >
           {selectedItem != '' ? selectedItem : 'What is your question about?'}
         </StyledButton>
+        <input
+          type="text"
+          id="question_topic"
+          value={this.state.selectedItem}
+          hidden
+        />
         <StyledDropDown isOpen={isOpen}>
           <List>
             <ListItem id="0" onClick={e => this.setSelectedDropdownItem(e)}>
