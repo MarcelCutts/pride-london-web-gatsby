f8671af475fdd08f14f5db0b80e01a2e3544a126
David Sheldrick <david@futurice.com>
2018-06-10 00:21:50 +0100

    fix deploy

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -3,4 +3,4 @@
 name=pride-in-london-$1
 
 now ./public -n $name -e NODE_ENV=production --token $NOW_TOKEN --team=prideinlondon
-now alias $name --token $NOW_TOKEN --team=prideinlondon
+now alias $name -n $name --token $NOW_TOKEN --team=prideinlondon
