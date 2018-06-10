6304fcfe614fde96726e704d7f4065d38333fc74
David Sheldrick <david@futurice.com>
2018-06-10 15:30:38 +0100

    update deploy script

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -7,6 +7,7 @@ export name=pride-in-london-$1
 
 echo "writing now.json file"
 echo "{\"name\": \"$name\", \"alias\": \"$name\"}" > now.json
+cat now.json
 
 echo "running now"
 now ./public -e NODE_ENV=production --token $NOW_TOKEN --team=prideinlondon
