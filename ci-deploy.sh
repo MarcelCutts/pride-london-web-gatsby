53216098dd80683b299dfba3f2e069d40d9b8406
David Sheldrick <david@futurice.com>
2018-06-10 15:18:45 +0100

    add logging to ci-deploy.sh

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -2,9 +2,14 @@
 
 set -e
 
+echo "deploying $1"
 export name=pride-in-london-$1
-git 
+
+echo "writing now.json file"
 echo "{\"name\": \"$name\", \"alias\": \"$name\"}" > now.json
 
+echo "running now"
 now ./public -e NODE_ENV=production --token $NOW_TOKEN --team=prideinlondon
+
+echo "running now alias"
 now alias --token $NOW_TOKEN --team=prideinlondon
